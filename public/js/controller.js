8ba73d557dd0ce513ff7f8d4a05d9e5cbbb25bfb
Roy Hasson <rhasson@gmail.com>
2013-06-11 19:17:14 -0400

    added register controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -37,6 +37,23 @@ YoApp.controller('yoOrdersCtrl', function($scope, service_orders) {
 
 });
 
+YoApp.controller('yoRegister', function($scope) {
+	$.post('/register/'+$('#iEmail').value(), {
+			email: $('#iEmail').value(),
+			firstname: $('#iFirstname').value(),
+			lastname: $('#iLastname').value(),
+			password: CryptoJS.MD5($('#iPassword').value()).toString()
+		})
+		.done(function(body, status, xhr) {
+			console.log(status, body);
+		}),
+		.fail(function(err) {
+			console.log(err);
+		});
+	});
+});
+
+
 //YoApp.directive('yoBasketDropdown')
 
 //YoApp.directive('yoCheckout')
