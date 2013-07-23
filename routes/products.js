b34fe4a1e7bad658a46114293b8bf95dafd8cf5d
rhasson <rhasson@gmail.com>
2013-07-22 22:30:43 -0400

    add product sku and thumbnail to admin page

--- routes/products.js
+++ routes/products.js
@@ -16,6 +16,11 @@ module.exports = exports = {
 	},
 	save: function(req, resp, next) {
 		if (req.session && 'name' in req.session) {
+			if ('thumb' in req.body && 'sku' in req.body) {
+				var l = req.body.sku[0].toUpperCase();
+				req.body.thumb = process.cwd() + '/public/img/' + l + '/thumb/' + req.body.thumb;
+				req.body.thumb_large = process.cwd() + '/public/img/' + l + '/thumb_large/' + req.body.thumb;
+			}
 			db.save('products', req.session.user_id, req.body)
 			.then(function(doc) {
 				resp.json(doc);
@@ -29,6 +34,11 @@ module.exports = exports = {
 	},
 	update: function(req, resp, next) {
 		if (req.session && 'name' in req.session) {
+			if ('thumb' in req.body && 'sku' in req.body) {
+				var l = req.body.sku[0].toUpperCase();
+				req.body.thumb = process.cwd() + '/public/img/' + l + '/thumb/' + req.body.thumb;
+				req.body.thumb_large = process.cwd() + '/public/img/' + l + '/thumb_large/' + req.body.thumb;
+			}
 			db.update('products', req.params.id, req.body)
 			.then(function(doc) {
 				resp.json(doc);
